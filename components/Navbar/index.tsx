086b98946093085ea7131bef8bd6899176110973
Andrey Mitko <andrey@mitko.me>
2022-09-23 20:02:31 +0100

    Initial Commit

---
+++ components/Navbar/index.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { Title } from "./styles";
+
+type Props = {};
+
+const Navbar = (props: Props) => {
+  return <Title>Navbar</Title>;
+};
+
+export default Navbar;
---
+++ styles/styles.tsx
@@ -0,0 +1,9 @@
+import styled, { createGlobalStyle, css } from "styled-components";
+
+export const GlobalStyle = createGlobalStyle`
+    
+    body {
+        background-color: white;
+    }
+    
+`;
