a9755026d5775103ddf87cdd924608525bdf4a87
Andrey Mitko <andrey@mitko.me>
2022-10-13 15:14:39 +0100

    Added gsap, demo hero animation

--- components/HeroSection/index.tsx
+++ components/HeroSection/index.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useRef } from "react";
 import {
   HeroText,
   HeroWrapper,
@@ -8,14 +8,35 @@ import {
   OrangeText,
   StyledArrowUp,
 } from "./styles";
+import { gsap } from "gsap";
 
 type Props = {};
 
 const HeroSection = (props: Props, ref: any) => {
+  const comp = useRef<HTMLDivElement | null>(null); // create a ref for the root level element (for scoping)
+
+  useEffect(() => {
+    // create our context. This function is invoked immediately and all GSAP animations and ScrollTriggers created during the execution of this function get recorded so we can revert() them later (cleanup)
+    let ctx = gsap.context(() => {
+      // all our animations can use selector text like ".box"
+      // and it's properly scoped to our component
+      gsap.set(".hero-text", { opacity: 0, y: 48 });
+      gsap.to(".hero-text", {
+        delay: 0.5,
+        duration: 1.5,
+        opacity: 1,
+        y: 0,
+        ease: "power3.out",
+      });
+    }, comp); // <- IMPORTANT! Scopes selector text
+
+    return () => ctx.revert(); // cleanup
+  }, []); // <- empty dependency Array so it doesn't re-run on every render
+
   return (
     <>
-      <HeroWrapper>
-        <HeroText>
+      <HeroWrapper ref={comp}>
+        <HeroText className="hero-text">
           I am a <BlueText>cr</BlueText>
           <PinkText>ea</PinkText>
           <GreenText>ti</GreenText>
