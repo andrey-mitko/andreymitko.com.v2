7e55d59f4924e8212ccdbc566863ad4ad588cbe9
Andrey Mitko <andrey@mitko.me>
2022-10-15 17:12:10 +0100

    Preped for google analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,12 +4,31 @@ import "normalize.css/normalize.css";
 import "remixicon/fonts/remixicon.css";
 import { gsap } from "gsap";
 import { ScrollTrigger } from "gsap/dist/ScrollTrigger";
+import Script from "next/script";
 
 gsap.registerPlugin(ScrollTrigger);
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
+      <Script
+        strategy="lazyOnload"
+        src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
+      />
+
+      {/* Check by typing "dataLayer" on client terminal */}
+      {/* https://enlear.academy/add-google-analytics-to-a-next-js-application-5525892844db */}
+
+      <Script strategy="lazyOnload" id="gtm-script">
+        {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}', {
+            page_path: window.location.pathname,
+            });
+          `}
+      </Script>
       <GlobalStyle />
       <Component {...pageProps} />
     </>
