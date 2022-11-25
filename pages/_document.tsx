aab2b409095c3de5decd6cc77d53317e323ee1b5
Andrey Mitko <andrey@mitko.me>
2022-11-25 21:35:54 +0000

    Prevent unstyled content flash

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,21 +1,54 @@
-import { Html, Head, Main, NextScript } from "next/document";
+import _Document, {
+  Html,
+  Head,
+  Main,
+  NextScript,
+  DocumentContext,
+} from "next/document";
+import { ServerStyleSheet } from "styled-components";
+export default class Document extends _Document {
+  // This removes the awkward flash of unstyled content.
+  static async getInitialProps(ctx: DocumentContext) {
+    const sheet = new ServerStyleSheet();
+    const originalRenderPage = ctx.renderPage;
+    try {
+      ctx.renderPage = () =>
+        originalRenderPage({
+          enhanceApp: (App) => (props) =>
+            sheet.collectStyles(<App {...props} />),
+        });
 
-export default function Document() {
-  return (
-    <Html lang="en" style={{ visibility: "hidden" }}>
-      <Head>
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" />
-        <link
-          href="https://fonts.googleapis.com/css2?family=Lora:wght@400;500;600;700&display=swap"
-          rel="stylesheet"
-        />
-        <link rel="stylesheet" href="https://use.typekit.net/vvq7ghc.css" />
-      </Head>
-      <body style={{ overflow: "hidden" }}>
-        <Main />
-        <NextScript />
-      </body>
-    </Html>
-  );
+      const initialProps = await _Document.getInitialProps(ctx);
+      return {
+        ...initialProps,
+        styles: (
+          <>
+            {initialProps.styles}
+            {sheet.getStyleElement()}
+          </>
+        ),
+      };
+    } finally {
+      sheet.seal();
+    }
+  }
+  render() {
+    return (
+      <Html lang="en" style={{ visibility: "hidden" }}>
+        <Head>
+          <link rel="preconnect" href="https://fonts.googleapis.com" />
+          <link rel="preconnect" href="https://fonts.gstatic.com" />
+          <link
+            href="https://fonts.googleapis.com/css2?family=Lora:wght@400;500;600;700&display=swap"
+            rel="stylesheet"
+          />
+          <link rel="stylesheet" href="https://use.typekit.net/vvq7ghc.css" />
+        </Head>
+        <body style={{ overflow: "hidden" }}>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
 }
